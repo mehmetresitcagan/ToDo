71da6dcfce5831963ce07ae079a0cec9e7f2e079
mehmetresitcagan <mresitcagan@gmail.com>
2024-03-02 20:01:14 +0300

    controller done

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -8,8 +8,19 @@ const getAllTasks = async (req, res) => {
     res.status(500).json({ msg: error });
   }
 };
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOne({ _id: taskId });
+    if (!task) {
+      return res
+        .status(404)
+        .json({ msg: `no matching task with id: ${taskId}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 const createTask = async (req, res) => {
   try {
@@ -22,8 +33,19 @@ const createTask = async (req, res) => {
 const updateTask = (req, res) => {
   res.send("update task");
 };
-const deleteTask = (req, res) => {
-  res.send("delete task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskId });
+    if (!task) {
+      return res
+        .status(404)
+        .json({ msg: `no matching task with id: ${taskId}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
